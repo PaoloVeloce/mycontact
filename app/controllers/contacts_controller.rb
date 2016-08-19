9c5d2930923f8436c83a38dd82558cf6a0a95c43
PaoloVeloce <pablo.veloce@gmail.com>
2016-08-19 15:42:43 +0300

    add new and create methods, make a add contact erb page and successfully add one contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,24 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
   end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Succesfully created new contact"
+      redirect_to contacts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :company, :email, :contactphone, :workphone, :address, :telegram, :whatsapp, :viber, :history)
+  end
 end
